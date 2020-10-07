9b5a45b5adab9ffa5e7bd73fc64978f67cb785f5
skyglass <skyglass2001@gmail.com>
2020-10-07 04:18:00 +0200

    CCDC-001
    - initial microservices commit

---
+++ todo-web-application-mysql/src/main/java/skyglass/composer/springboot/web/service/TodoRepository.java
@@ -0,0 +1,18 @@
+package skyglass.composer.springboot.web.service;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import skyglass.composer.springboot.web.model.Todo;
+
+public interface TodoRepository extends JpaRepository<Todo, Integer>{
+	List<Todo> findByUser(String user);
+	
+	//service.retrieveTodos(name)
+
+	//service.deleteTodo(id);
+	//service.retrieveTodo(id)
+	//service.updateTodo(todo)
+	//service.addTodo(getLoggedInUserName(model), todo.getDesc(), todo.getTargetDate(),false);
+}
